e7a5d8ea0f827f33936694b9ba083a994d2bd02c
hayden <haydenwb@gmail.com>
2019-09-05 16:19:36 +0100

    minor refactor removing old code

--- js/app.js
+++ js/app.js
@@ -1,17 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
-
-
   const handleFormSubmit = function (event) {
     event.preventDefault();
 
     const bookTitle = event.target.title.value
     const authorName = event.target.author.value
     const categorySelected = event.target.category.value
-    console.log(bookTitle);
-    console.log(authorName);
-    console.log(categorySelected);
     
     const readingListDiv = document.querySelector('#reading-list')
     const newParagraphItem = document.createElement('p')
@@ -24,19 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
+
 })
-
-// const unorderedList = document.querySelector('ul')
-// const newListItem = document.createElement('li')
-// newListItem.textContent = "Purple"
-// newListItem.classList.add('purple', "bold")
-// unorderedList.appendChild(newListItem)
-
-
-// newListItem.textContent = "Purple"
-// newListItem.classList.add('purple', "bold")
-
-
-// 1. Title - text input
-// 2. Author - text input
-// 3. Category(e.g.book, article, blog post) - select
